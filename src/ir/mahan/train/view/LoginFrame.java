2acc8e8533ccda01b83a485dbbe1580ecd8b7c24
mehrnaz.vafi <mehrnaz.vafi@Dev.MahanAir>
2015-08-14 10:22:29 +0430

    login frame started

---
+++ src/ir/mahan/train/view/LoginFrame.java
@@ -0,0 +1,12 @@
+package ir.mahan.train.view;
+
+import javax.swing.JFrame;
+
+public class LoginFrame extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
