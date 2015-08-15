980626344e445aabe19aa305aeb38cd524d35d29
mehrnaz.vafi <mehrnaz.vafi@Dev.MahanAir>
2015-08-15 09:46:24 +0430

    numerci textf field class added

---
+++ src/ir/mahan/train/view/NumericField.java
@@ -0,0 +1,11 @@
+package ir.mahan.train.view;
+
+import javax.swing.JTextField;
+
+public class NumericField extends JTextField{
+	@Override
+	public void setText(String arg0) {
+		// TODO Auto-generated method stub
+		super.setText(arg0);
+	}
+}
