cfbc901f133c6cb240815573e72f4fc723ddf0b6
reihaneh.heidari <reihaneh.heidari@VWS06.Dev.MahanAir>
2015-08-23 12:30:30 +0430

    some Changin in loginpanel class

--- src/ir/mahan/train/view/LoginPanel.java
+++ src/ir/mahan/train/view/LoginPanel.java
@@ -1,15 +1,18 @@
 package ir.mahan.train.view;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import ir.mahan.train.controller.Controller;
 
+import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -43,6 +46,10 @@ public class LoginPanel extends JPanel {
 		userNametf = new JTextField();
 		emptyPasswordStar = new JLabel("*");
 		emptyUsernameStar = new JLabel("*");
+		emptyPasswordStar.setFont(new Font("Courier New", Font.BOLD, 18));
+		emptyPasswordStar.setForeground(Color.RED);
+		emptyUsernameStar.setFont(new Font("Courier New", Font.BOLD, 18));
+		emptyUsernameStar.setForeground(Color.RED);
 		emptyPasswordStar.setVisible(false);
 		emptyUsernameStar.setVisible(false);
 		userNametf.addActionListener(new ActionListener() {
@@ -60,7 +67,7 @@ public class LoginPanel extends JPanel {
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				
+				loginBtn.requestFocusInWindow();
 				
 			}
 		});
@@ -119,6 +126,27 @@ public class LoginPanel extends JPanel {
 		gc.anchor = GridBagConstraints.NORTH;
 		this.add(loginBtn, gc);
 		loginBtn.addActionListener(new LoginBtnClick());
+		loginBtn.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode()==KeyEvent.VK_ENTER)
+				{
+					 loginBtn.doClick();
+					
+				}	
+			}
+		} );
 
 		gc.gridy = 3;
 		gc.gridx = 2;
@@ -128,11 +156,12 @@ public class LoginPanel extends JPanel {
 
 	}
 
-	private class LoginBtnClick implements ActionListener {
+	private class LoginBtnClick implements ActionListener  {
 
 		@Override
 		public void actionPerformed(ActionEvent event) {
 			JButton btn = (JButton) event.getSource();
+			btn.requestFocusInWindow();
 			String password = "";
 			if (btn == loginBtn) {
 				if (iLoginListener != null) {
@@ -141,13 +170,26 @@ public class LoginPanel extends JPanel {
 					for (int i = 0; i < passwordarr.length; i++) {
 						password += passwordarr[i];
 					}
-					if (username.isEmpty()) {
+					if(username.isEmpty() & password.isEmpty()){
 						userNametf.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.red));
 						emptyUsernameStar.setVisible(true);
-						JOptionPane.showMessageDialog(null,"usename can not be empty!", "Error", JOptionPane.ERROR_MESSAGE);
+						passwordFd.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.red));
+						emptyPasswordStar.setVisible(true);
+						JOptionPane.showMessageDialog(null,"Please Enter UserName And Password", "Error", JOptionPane.ERROR_MESSAGE);
+					}
+					else if (username.isEmpty()) {
+						userNametf.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.red));
+						emptyUsernameStar.setVisible(true);
+						JOptionPane.showMessageDialog(null,"Usename Can Not be empty!", "Error", JOptionPane.ERROR_MESSAGE);
+						passwordFd.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+						emptyPasswordStar.setVisible(false);
 					}
 					else if (password.isEmpty()) {
-						JOptionPane.showMessageDialog(null,"password can not be empty!", "Error", JOptionPane.ERROR_MESSAGE);
+						passwordFd.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.red));
+						emptyPasswordStar.setVisible(true);
+						JOptionPane.showMessageDialog(null,"Password can not be empty!", "Error", JOptionPane.ERROR_MESSAGE);
+						userNametf.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+						emptyUsernameStar.setVisible(false);
 					} else {
 					iLoginListener.userInfoEmit(username, password);
 					}
@@ -155,5 +197,6 @@ public class LoginPanel extends JPanel {
 			}
 
 		}
+
 	}
 }
